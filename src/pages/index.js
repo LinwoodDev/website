73cb4e7323c56996192c490a9bb13beda1a81f44
CodeDoctor <wusel905@gmail.com>
2021-02-04 20:02:11 +0100

    change layout description

--- src/pages/index.js
+++ src/pages/index.js
@@ -57,7 +57,7 @@ function Home() {
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="Welcome on the Linwood website!">
       <header className={clsx('hero hero--primary', styles.heroBanner)}>
         <div className="container">
           <h1 className="hero__title">{siteConfig.title}</h1>
