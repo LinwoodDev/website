3233dfcd2b34e9c5714fab0089ec0eab6ecb2295
CodeDoctor <wusel905@mail.com>
2021-08-16 10:57:43 +0200

    add pwa

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
+const withPWA = require('next-pwa')
+
 /** @type {import('next').NextConfig} */
-module.exports = {
+module.exports = withPWA({
+  pwa: {
+    dest: 'public'
+  },
   reactStrictMode: true,
-}
+})
