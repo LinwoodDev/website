0d8bbe10f46ff6158727f4b3ba65362b68d7f5a2
CodeDoctorDE <janandrejdiehl@gmail.com>
2021-08-25 18:48:20 +0200

    add runtime caching

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 const withPWA = require('next-pwa')
+const runtimeCaching = require('next-pwa/cache')
 
 /** @type {import('next').NextConfig} */
 module.exports = withPWA({
   pwa: {
-    dest: 'public'
+    dest: 'public',
+    runtimeCaching,
   },
   images: {
     domains: ['avatars1.githubusercontent.com'],
