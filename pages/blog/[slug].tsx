a80e1f83db23bb4deb677e19f4d406ae3105cfd3
CodeDoctorDE <codedoctor@linwood.dev>
2022-03-14 18:37:54 +0100

    migrate to reactmarkdown

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -8,7 +8,9 @@ import React from 'react'
 import Navbar from '../../components/LinwoodHeader'
 import Footer from '../../components/Footer'
 import AuthorDisplay from '../../components/AuthorDisplay'
-import { Box, Container, Image, Space, Text, Title } from '@mantine/core'
+import { Blockquote, Box, Container, Image, List, Space, Text, Title } from '@mantine/core'
+import ReactMarkdown from 'react-markdown'
+import Link from '../../components/Link'
 
 type Props = {
   post: PostType
@@ -34,7 +36,7 @@ const Post = ({ post }: Props) => {
               </title>
               <meta property="og:image" content={post.ogImage?.url} />
             </Head>
-            <Container size="sm" style={{minHeight: "100vh"}}>
+            <Container size="sm" style={{ minHeight: "100vh" }}>
               {post.coverImage &&
                 <Image src={post.coverImage} alt="Post logo" />
               }
@@ -44,7 +46,22 @@ const Post = ({ post }: Props) => {
               </Box>
               <Text>{post.date}</Text>
               <Text>
-                <div dangerouslySetInnerHTML={{ __html: post.content }} />
+                <ReactMarkdown components={{
+                  a: (props) => <Link {...props} />,
+                  h1: (props) => <Title order={1} {...props} />,
+                  h2: (props) => <Title order={2} {...props} />,
+                  h3: (props) => <Title order={3} {...props} />,
+                  h4: (props) => <Title order={4} {...props} />,
+                  h5: (props) => <Title order={5} {...props} />,
+                  h6: (props) => <Title order={6} {...props} />,
+                  p: (props) => <Text {...props} />,
+                  li: (props) => <List.Item {...props} />,
+                  ul: (props) => <List {...props} />,
+                  ol: (props) => <List {...props} type={"ordered"} />,
+                  blockquote: (props) => <Blockquote {...props} />,
+                  // eslint-disable-next-line jsx-a11y/alt-text
+                  img: (props) => <Image {...props} />,
+                }}>{post.content}</ReactMarkdown>
               </Text>
             </Container>
           </article>
@@ -73,8 +90,6 @@ export async function getStaticProps({ params }: Params) {
     'ogImage',
     'coverImage',
   ])
-  const content = await markdownToHtml(post.content || '')
-  post['content'] = content;
 
   return {
     props: {
