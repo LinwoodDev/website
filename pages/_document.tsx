63c10f929b378e6fe797e14855123ad5f1e6d41c
CodeDoctorDE <codedoctor@linwood.dev>
2022-03-25 20:11:47 +0100

    Add pwa to document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,8 +1,48 @@
-import Document from "next/document";
+import Document, { Head, Html, Main, NextScript } from "next/document";
 import { createGetInitialProps } from "@mantine/next";
 
 const getInitialProps = createGetInitialProps();
 
 export default class _Document extends Document {
   static getInitialProps = getInitialProps;
+  
+  render() {
+    return (
+      <Html lang='en' dir='ltr'>
+        <Head>
+          <meta name='application-name' content={"Linwood Development"} />
+          <meta name='apple-mobile-web-app-capable' content='yes' />
+          <meta name='apple-mobile-web-app-status-bar-style' content='default' />
+          <meta name='apple-mobile-web-app-title' content={"Linwood Development"} />
+          <meta name='description' content={"Linwood home page"} />
+          <meta name='format-detection' content='telephone=no' />
+          <meta name='mobile-web-app-capable' content='yes' />
+          <meta name='theme-color' content='#35ef53' />
+          {/* TIP: set viewport head meta tag in _app.js, otherwise it will show a warning */}
+          {/* <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover' /> */}
+          
+          <link rel='apple-touch-icon' sizes='180x180' href='/icons/apple-touch-icon.png' />
+          <link rel='manifest' href='/manifest.json' />
+          <link rel='shortcut icon' href='/favicon.ico' />
+          <style>{
+            `
+            html, body, #__next {
+              height: 100%;
+            }
+            #__next {
+              margin: 0 auto;
+            }
+            h1 {
+              text-align: center;
+            }
+            `
+          }</style>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
