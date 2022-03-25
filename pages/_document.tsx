50db8c8bf6d6d1c90eaaa80d7d245cc0c37331d0
CodeDoctorDE <codedoctor@linwood.dev>
2022-03-25 20:13:51 +0100

    format

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,27 +5,36 @@ const getInitialProps = createGetInitialProps();
 
 export default class _Document extends Document {
   static getInitialProps = getInitialProps;
-  
+
   render() {
     return (
-      <Html lang='en' dir='ltr'>
+      <Html lang="en" dir="ltr">
         <Head>
-          <meta name='application-name' content={"Linwood Development"} />
-          <meta name='apple-mobile-web-app-capable' content='yes' />
-          <meta name='apple-mobile-web-app-status-bar-style' content='default' />
-          <meta name='apple-mobile-web-app-title' content={"Linwood Development"} />
-          <meta name='description' content={"Linwood home page"} />
-          <meta name='format-detection' content='telephone=no' />
-          <meta name='mobile-web-app-capable' content='yes' />
-          <meta name='theme-color' content='#35ef53' />
+          <meta name="application-name" content={"Linwood Development"} />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="default"
+          />
+          <meta
+            name="apple-mobile-web-app-title"
+            content={"Linwood Development"}
+          />
+          <meta name="description" content={"Linwood home page"} />
+          <meta name="format-detection" content="telephone=no" />
+          <meta name="mobile-web-app-capable" content="yes" />
+          <meta name="theme-color" content="#35ef53" />
           {/* TIP: set viewport head meta tag in _app.js, otherwise it will show a warning */}
           {/* <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover' /> */}
-          
-          <link rel='apple-touch-icon' sizes='180x180' href='/icons/apple-touch-icon.png' />
-          <link rel='manifest' href='/manifest.json' />
-          <link rel='shortcut icon' href='/favicon.ico' />
-          <style>{
-            `
+
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/icons/apple-touch-icon.png"
+          />
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="shortcut icon" href="/favicon.ico" />
+          <style>{`
             html, body, #__next {
               height: 100%;
             }
@@ -35,14 +44,13 @@ export default class _Document extends Document {
             h1 {
               text-align: center;
             }
-            `
-          }</style>
+            `}</style>
         </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
