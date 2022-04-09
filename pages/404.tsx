3c9573d10c229b88d0c88c4d12c2149619745cf2
CodeDoctorDE <codedoctor@linwood.dev>
2022-04-09 13:13:42 +0200

    Design soon and 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,13 +1,36 @@
-import { Box, Button, Container, Group, Text, Title } from "@mantine/core";
+import {
+  Box,
+  Button,
+  Container,
+  createStyles,
+  Group,
+  Text,
+  Title,
+} from "@mantine/core";
 import { NextLink } from "@mantine/next";
 import Head from "next/head";
 import React from "react";
 import Footer from "../components/Footer";
 import LinwoodHeader from "../components/LinwoodHeader";
 
+const useStyles = createStyles((theme) => ({
+  label: {
+    fontSize: "300px",
+    fontWeight: "bold",
+    color: theme.colors.primary,
+    [theme.fn.smallerThan("md")]: {
+      fontSize: "100px",
+    },
+    [theme.fn.smallerThan("sm")]: {
+      fontSize: "50px",
+    },
+  },
+}));
+
 type Props = {};
 
 export default function NotFoundPage({}: Props) {
+  const { classes } = useStyles();
   return (
     <>
       <Head>
@@ -24,10 +47,12 @@ export default function NotFoundPage({}: Props) {
       <main>
         <Container size={"lg"} style={{ minHeight: "100vh" }}>
           <Box sx={{ marginTop: "10em" }}>
-            <Title align="center" order={1}>
+            <Title className={classes.label} align="center" order={1}>
               404
             </Title>
-            <Text align="center">The link you clicked does not exist.</Text>
+            <Text color="dimmed" size="lg" align="center">
+              The link you clicked does not exist.
+            </Text>
             <Group spacing={3} position="center" mt={16}>
               <Button component={NextLink} href="/" color="green">
                 Back to home
