9b742f06449960a60da9f25631266121b90bd3ad
CodeDoctor <wusel905@gmail.com>
2022-07-27 14:10:14 +0200

    Fix link again

--- components/Link.tsx
+++ components/Link.tsx
@@ -3,9 +3,7 @@ import NextLink from "next/link";
 import React, { PropsWithChildren } from "react";
 
 export default function Link(
-  props: PropsWithChildren<
-    React.ComponentProps<typeof MantineAnchor> & { href?: string }
-  >
+  props: PropsWithChildren<React.ComponentPropsWithoutRef<"a"> & AnchorProps>
 ) {
   return (
     <NextLink href={props.href ?? ""} passHref>
