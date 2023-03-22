22be64d7447f87a07fee32de7df71af9d2af558e
CodeDoctorDE <codedoctor@linwood.dev>
2023-03-22 21:14:51 +0100

    Add progress plugin

--- src/scripts/swup.js
+++ src/scripts/swup.js
@@ -1,8 +1,8 @@
 import Swup from 'swup';
 import SwupHeadPlugin from '@swup/head-plugin';
-
+import SwupProgressPlugin from '@swup/progress-plugin';
 
 const swup = new Swup({
-    plugins: [new SwupHeadPlugin()]
+    plugins: [new SwupHeadPlugin(), new SwupProgressPlugin()]
   });
   
