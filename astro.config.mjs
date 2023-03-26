41d12f2e785148d2291219dcc0a8b2d20aa90c45
CodeDoctorDE <codedoctor@linwood.dev>
2023-03-26 09:43:26 +0200

    Add pwa

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,40 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import AstroPWA from "@vite-pwa/astro";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://linwood.dev',
-	integrations: [mdx(), sitemap()],
+  site: "https://linwood.dev",
+  integrations: [
+    mdx(),
+    sitemap(),
+    AstroPWA({
+      workbox: { navigateFallback: "/404" },
+      manifest: {
+        name: "Linwood Development",
+        short_name: "Linwood",
+        description: "Simple to use software for everyone",
+        theme_color: "#35EF53",
+		icons: [
+			{
+			  src: 'android-chrome-192x192.png',
+			  sizes: '192x192',
+			  type: 'image/png'
+			},
+			{
+			  src: 'android-chrome-512x512.png',
+			  sizes: '512x512',
+			  type: 'image/png'
+			},
+			{
+			  src: 'android-chrome-512x512.png',
+			  sizes: '512x512',
+			  type: 'image/png',
+			  purpose: 'any maskable'
+			}
+		  ]		  
+      },
+    }),
+  ],
 });
