13d97d7b972ea59f565be2522c5692ac041f2044
CodeDoctorDE <codedoctor@linwood.dev>
2024-03-11 12:31:47 +0100

    Fix typo

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -3,7 +3,7 @@ import type { APIContext } from 'astro';
 import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
-export async function get(context : APIContext) {
+export async function GET(context : APIContext) {
 	const posts = await getCollection('blog');
 	return rss({
 		title: SITE_TITLE,
