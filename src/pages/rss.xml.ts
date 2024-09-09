8d18becb0fb1b213a3fa235e1ce8765b7fd68def
CodeDoctorDE <codedoctor@linwood.dev>
2024-09-09 17:37:53 +0200

    Add and fix rss entry, fix grammar, add 2.2.0-beta.3 butterfly entry

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,19 +1,20 @@
-import rss from '@astrojs/rss';
-import type { APIContext } from 'astro';
-import { getCollection } from 'astro:content';
-import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
+import rss from "@astrojs/rss";
+import type { APIContext } from "astro";
+import { getCollection } from "astro:content";
+import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
+import { getEntryUrl } from "src/blog";
 
-export async function GET(context : APIContext) {
-	const posts = await getCollection('blog');
-	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: context.site?.toString() ?? '',
-		items: posts.map((post) => ({
-			link: `/blog/${post.slug}/`,
-			pubDate: post.data.date,
-			title: post.data.title,
-			description: post.data.description,
-		})),
-	});
+export async function GET(context: APIContext) {
+  const posts = await getCollection("blog");
+  return rss({
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: context.site?.toString() ?? "",
+    items: posts.map((post) => ({
+      link: getEntryUrl(post),
+      pubDate: post.data.date,
+      title: post.data.title,
+      description: post.data.description,
+    })),
+  });
 }
